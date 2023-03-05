038b7705fdaf202ca5cf34981959ba98de968a14
RomeoFroger49 <romeo.froger@gmail.com>
2023-03-05 20:18:15 +0100

    js check and replace input done

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 
-const name_t = document.getElementById('name');
+
 
 document.getElementById('name-e').addEventListener("input", function replace(e){
-    name_t.textContent = e.target.value;
+    document.getElementById('name').textContent = e.target.value;
 });
 
 const chiffre_t = document.getElementById('chiffre');
 
 document.getElementById('chiffre-e').addEventListener("input", replaceChiffre);
+document.getElementById('chiffre-e').myPara = document.getElementById('chiffre');
+document.getElementById('chiffre-e').myNumber = 16;
+document.getElementById('chiffre-e').myP = document.getElementById("chiffre-alert");
+
+
+(document.getElementById('year-e')).addEventListener("input", replaceChiffre);
+(document.getElementById('year-e')).myPara = document.getElementById('year');
+(document.getElementById('year-e')).myNumber = 2;
+document.getElementById('year-e').myP = document.getElementById("year-alert");
+
+(document.getElementById('month-e')).addEventListener("input", replaceChiffre);
+(document.getElementById('month-e')).myPara = document.getElementById('month');
+(document.getElementById('month-e')).myNumber = 2;
+document.getElementById('month-e').myP = document.getElementById("year-alert");
+
+(document.getElementById('cvc-e')).addEventListener("input", replaceChiffre);
+(document.getElementById('cvc-e')).myPara = document.getElementById('cvc');
+(document.getElementById('cvc-e')).myNumber = 3;
+document.getElementById('cvc-e').myP = document.getElementById("year-alert");
+
+
 
 function replaceChiffre(e){
     const pattern =/[a-zA-Z]/g;
-    if(pattern.test(e.target.value) || e.target.value.length > 16){
+    if(pattern.test(e.target.value) || e.target.value.length > e.target.myNumber){
         e.target.style.border = "solid red";
         e.target.style.color = "red";
-        document.getElementById("alert-message").style.display = "block";
-        chiffre_t.textContent = e.target.value;
+        e.target.myP.style.display = "block";
+        e.target.myPara.textContent = e.target.value;
         
     }else{
-        document.getElementById("alert-message").style.display = "none";
+        e.target.myP.style.display = "none";
         e.target.style = "";
-        chiffre_t.textContent = e.target.value;
+        e.target.myPara.textContent = e.target.value;
     }
     
 }
